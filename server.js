90363c97dd4fdb3de43c1975c9420dedf18d1f60
Eric Song <eric.song@rutgers.edu>
2014-06-14 01:49:52 -0400

    udpated callback URL

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
+    callbackURL: "http://samshreds:3500/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
 
